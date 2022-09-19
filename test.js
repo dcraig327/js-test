310d380feb3f75e682997b2cce61bccd5a30d326
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-19 03:16:04 -0700

    add new test to print variable name
    not entirely functional as I'd like. I want a simple function to pass
    a variable and it print the name of that variable and the value

--- test.js
+++ test.js
@@ -1,4 +1,45 @@
-debugger;
-for (let i=0; i<2; i++) {
-    console.log(i);
+const varToString = (varObj) => Object.keys(varObj)[0];
+function peekVar(variable) {
+  console.log(`${variable}`);
+  console.log(varToString({ variable }));
 }
+// let z = Object(x);
+// let z = Object(x);
+///////////////////////
+// console.log(`${}: ${x}`); //0
+// pname(y);
+
+// peekVar(x);
+// peekVar(y);
+
+// console.log(`y: ${y}`); //0
+
+function foo() {
+  let x = 0;
+  var y = 0;
+  //  let z = Object(x).keys()[0];
+  //  let z = Object.create(x);
+  console.log(z);
+  {
+    let x = 1;
+    var y = 1;
+    console.log(`x,y: ${x},${y}`); //0,0
+    {
+      // console.log(x); //error
+      let x = 2;
+      var y = 2;
+      console.log(`x,y: ${x},${y}`); //0,0
+      {
+        // console.log(x); //error
+        let x = 3;
+        var y = 2;
+        console.log(`x,y: ${x},${y}`); //0
+      }
+      console.log(`x,y: ${x},${y}`); //0
+    }
+    console.log(`x,y: ${x},${y}`); //0
+  }
+  console.log(`x,y: ${x},${y}`); //0
+}
+
+foo();
